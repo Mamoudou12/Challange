b8da2c01c2dd85c01103163e6801f0e00ec4b942
Mamoudou12 <bmamoudou463@gmail.com>
2024-04-27 22:52:06 +0000

    la possibilité à l'utilisateur de saisir 3 nombre

--- addition.js
+++ addition.js
@@ -1,8 +1,14 @@
 function calculate(event) {
-    event.preventDefault(); // Empêche la soumission du formulaire
-  
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let result = num1 + num2;
-    document.getElementById('result').innerText = 'Résultat : ' + result;
+  event.preventDefault(); 
+
+  let num1 = parseFloat(document.getElementById('num1').value);
+  let num2 = parseFloat(document.getElementById('num2').value);
+  let num3 = parseFloat(document.getElementById('num3').value);
+
+  if (isNaN(num3)) {
+    num3 = 0; 
   }
+
+  let result = num1 + num2 + num3;
+  document.getElementById('result').innerText = 'Résultat : ' + result;
+}
